a2dfdeed050293c47924a1ba78c7a82c5b0270d5
Atta_ur_rehman <attaurr786@outlook.com>
2023-05-31 21:15:11 +0500

    First commit

---
+++ contactuspage/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+
+const Navigation = () => {
+  return (
+    <nav className="container">
+        <div className="logo">
+            <img src="images/logo.png" alt="Logo" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation
